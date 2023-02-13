dc24e553069f156aed83d9513bcf6ba9a0af4c89
Yufanabc <f.yufan@hotmail.com>
2023-02-12 18:28:45 -0800

    keejf

--- __a51.bat
+++ __a51.bat
@@ -1,5 +1,12 @@
 @echo off
+<<<<<<< Updated upstream
 C:
 cd "\Users\steph\OneDrive\Desktop\UBC\3rd Year\ELEC 291\elec291_project1\"
 "C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\steph\OneDrive\Desktop\UBC\3rd Year\ELEC 291\elec291_project1\project_1_code.asm"
 echo Crosside_Action Set_Hex_File C:\Users\steph\OneDrive\Desktop\UBC\3rd Year\ELEC 291\elec291_project1\project_1_code.HEX
+=======
+D:
+cd "\Program\ELEC 291\Project 1\elec291_project1\"
+"D:\CrossIDE\Call51\Bin\a51.exe" -l "D:\Program\ELEC 291\Project 1\elec291_project1\project_1_code.asm"
+echo Crosside_Action Set_Hex_File D:\Program\ELEC 291\Project 1\elec291_project1\project_1_code.HEX
+>>>>>>> Stashed changes
